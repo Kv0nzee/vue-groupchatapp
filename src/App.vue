589a2c95f2f7107d90d87f9eeb4b036b01d41ba1
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-02-03 17:21:36 +0630

    nprogress

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,17 @@ body{
 }
 
 #nprogress .bar {
-  height:5.5em;
-  background: #00a2ff !important;
+  height:0.5em;
+  background: #3d3d3d !important;
 }
 
 #nprogress .peg {
-  box-shadow: 0 0 10px #00a2ff, 0 0 5px #00a2ff;
+  box-shadow: 0 0 10px #3d3d3d, 0 0 5px #3d3d3d;
 }
 
 #nprogress .spinner-icon {
-  border-top-color: #00a2ff;
-  border-left-color: #00a2ff;
+  border-top-color: #3d3d3d;
+  border-left-color: #3d3d3d;
 }
 
 ::-webkit-scrollbar {
